af8950d049467ac84c54a51301e7330c55a6fe6e
Len Budney <Len.Budney@grantstreet.com>
2021-02-06 16:14:30 -0500

    Changed entrypoint script to use configurable UID/GIDs

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,46 @@ set -e
 # Files created by Elasticsearch should always be group writable too
 umask 0002
 
+# Make the UID/GID in the container match the desired UID/GID from
+# the host
+USER=elasticsearch
+OLD_UID="$( id -u ${USER} )"
+OLD_GID="$( id -g ${USER} )"
+CHANGED=""
+
+if [ -z "${USER_UID}" ]; then
+  USER_UID="${OLD_UID}"
+fi
+
+if [ -z "${USER_GID}" ]; then
+  USER_GID="${OLD_GID}"
+fi
+
+## Change GID for USER?
+if [ -n "${USER_GID}" ] && [ "${USER_GID}" != "${OLD_GID}" ]; then
+    sed -i -e "s/^${USER}:\([^:]*\):[0-9]*/${USER}:\1:${USER_GID}/" /etc/group
+    sed -i -e "s/^${USER}:\([^:]*\):\([0-9]*\):[0-9]*/${USER}:\1:\2:${USER_GID}/" /etc/passwd
+    CHANGED="1"
+fi
+
+## Change UID for USER?
+if [ -n "${USER_UID}" ] && [ "${USER_UID}" != "${OLD_UID}" ]; then
+    sed -i -e "s/^${USER}:\([^:]*\):[0-9]*:\([0-9]*\)/${USER}:\1:${USER_UID}:\2/" /etc/passwd
+    CHANGED="1"
+fi
+
+## Change ownership of user's files
+if [ ! -z "$CHANGED" ]; then
+    find / \
+        \( -uid "${OLD_UID}" -o -gid "${OLD_GID}" \) -not \
+        \( -path /proc\* -o -path /tmp\* \) \
+        -exec chown "${USER_UID}:${USER_GID}" {} \;
+fi
+
 run_as_other_user_if_needed() {
     if [[ "$(id -u)" == "0" ]]; then
         # If running as root, drop to specified UID and run command
-        exec chroot --userspec=1001 / "${@}"
+        exec chroot --userspec=${USER_UID} / "${@}"
     else
         # Either we are running in Openshift with random uid and are a member of the root group
         # or with a custom --user
@@ -30,8 +66,8 @@ if [[ "$1" != "eswrapper" ]]; then
         # Without this, user could specify `elasticsearch -E x.y=z` but
         # `bin/elasticsearch -E x.y=z` would not work.
         set -- "elasticsearch" "${@:2}"
-        # Use chroot to switch to UID 1001
-        exec chroot --userspec=1001 / "$@"
+        # Use chroot to switch to UID
+        exec chroot --userspec=${USER_UID} / "$@"
     else
         # User probably wants to run something else, like /bin/bash, with another uid forced (Openshift?)
         exec "$@"
@@ -93,7 +129,7 @@ fi
 if [[ "$(id -u)" == "0" ]]; then
     # If requested and running as root, mutate the ownership of bind-mounts
     if [[ -n "$TAKE_FILE_OWNERSHIP" ]]; then
-        chown -R 1001:0 /usr/share/elasticsearch/{data,logs}
+        chown -R ${USER_UID}:${USER_GID} /usr/share/elasticsearch/{data,logs}
     fi
 fi
 
